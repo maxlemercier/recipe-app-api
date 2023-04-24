e0d6913d934966c5ed626cc5802f5bd2508ac71c
Maxence Lemercier <maxence.lemercier@gmail.com>
2023-04-24 19:44:17 +0900

    initialize wait_for_db function

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,11 @@
+"""
+Django command to wait for the DB to be available
+"""
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        pass
